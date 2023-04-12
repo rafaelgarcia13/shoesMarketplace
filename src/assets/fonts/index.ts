618569886b84f73fb29f849d7d7f2fb4aa6e3e0e
Rafael Garcia <rafael.garcia@dtidigital.com.br>
2023-04-12 11:56:00 -0300

    feat: create file and configuration of custom fonts

---
+++ src/assets/fonts/index.ts
@@ -0,0 +1,15 @@
+interface IFont {
+  light: string;
+  regular: string;
+  medium: string;
+  bold: string;
+}
+
+const fonts: IFont = {
+  light: 'Roboto-Light',
+  regular: 'Roboto-Regular',
+  medium: 'Roboto-Medium',
+  bold: 'Roboto-Bold',
+};
+
+export { fonts };
