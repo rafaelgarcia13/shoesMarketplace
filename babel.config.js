08288aa9e0b846eb9e880025ad0d858a8aa3747f
Rafael Garcia <rafael.garcia@dtidigital.com.br>
2023-04-12 11:53:28 -0300

    feat: configuring typescript and babel modules

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,16 @@
 module.exports = {
   presets: ['module:metro-react-native-babel-preset'],
+  plugins: [
+    [
+      'module-resolver',
+      {
+        root: ['./src'],
+        extensions: ['.ios.js', '.android.js', '.js', '.ts', '.tsx', '.json'],
+        alias: {
+          '@fontFamily': './src/assets/fonts/index.ts',
+        },
+      },
+    ],
+    'react-native-reanimated/plugin', //let reanimated plugin at last
+  ],
 };
