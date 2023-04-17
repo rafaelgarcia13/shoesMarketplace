4c3c22a7a2c50309b41df2c2287f3f9de5494498
Rafael Garcia <rafael.garcia@dtidigital.com.br>
2023-04-17 17:08:56 -0300

    feat: shoes svgs

---
+++ src/assets/images/svg/index.ts
@@ -0,0 +1,17 @@
+import BlueShoes from './files/blueShoes.svg';
+import DarkBlueShoes from './files/darkBlueShoes.svg';
+import LightBlueShoes from './files/lightBlueShoes.svg';
+import RedBlackShoes from './files/redBlackShoes.svg';
+import RedShoes from './files/redShoes.svg';
+import SilverShoes from './files/silverShoes.svg';
+import WhiteRedShoes from './files/whiteRedShoes.svg';
+
+export {
+  BlueShoes,
+  DarkBlueShoes,
+  LightBlueShoes,
+  RedBlackShoes,
+  RedShoes,
+  SilverShoes,
+  WhiteRedShoes,
+};
